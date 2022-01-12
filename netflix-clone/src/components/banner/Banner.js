25f0f2766f4fd4053d9585c85f5bdee6bbd57da6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-12 16:29:19 -0500

    Starting to work on the NavBar component

--- netflix-clone/src/components/banner/Banner.js
+++ netflix-clone/src/components/banner/Banner.js
@@ -30,6 +30,7 @@ const Banner = ({ title }) => {
         </div>
         <h1 className='banner-description'>{truncate(movie?.overview, 150)}</h1>
       </div>
+      <div className='banner-fadeBottom'></div>
     </header>
   )
 }
