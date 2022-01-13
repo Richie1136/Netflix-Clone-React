122b3bf4b8462e561a9bbc397351d460ceeec8e3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-13 13:43:06 -0500

    Adding a black banner if the user scrolls at least 100px from the top

--- netflix-clone/src/components/navbar/NavBar.js
+++ netflix-clone/src/components/navbar/NavBar.js
@@ -1,9 +1,25 @@
 import './NavBar.css'
+import { useEffect, useState } from 'react'
 
 const NavBar = () => {
-  return (
-    <div>
 
+  const [handleShow, setHandleShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.screenY > 100) {
+        setHandleShow(true);
+      } else setHandleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    }
+  }, [])
+
+  return (
+    <div className={`nav ${handleShow && 'nav-black'}`}>
+      <img className='nav-logo' src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg' alt='Netflix logo' />
+      <img className='nav-avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='Netflix logo' />
     </div>
   )
 }
